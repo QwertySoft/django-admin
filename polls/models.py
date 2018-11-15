0c3d7992ee88c34cb8202504f19211e977c6f9e4
Máximo Zarza <zmaximo1990@gmail.com>
2018-11-15 18:14:37 -0300

    Add code comments

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,17 @@
+# Importamos los móculos que necesitamos
 from django.db import models
-from django.utils.timezone import now
 
+# Definimos el modelo Question
 class Question(models.Model):
-    name = models.CharField('Nombre', max_length=255, blank=True, null=True)
-    pub_date = models.DateTimeField('Fecha de publicacion', blank=True, null=True, default=now)
+    # Definimos los campos del modelo Question
+    name = models.CharField('Nombre', max_length=10, blank=False, null=False, default='')
+    pub_date = models.DateTimeField('Fecha de publicación', blank=True, null=True, auto_now_add=True)
 
+    # Definimos la clase Meta para el modelo Question
     class Meta:
+        # Definimos el orden por defecto al recuperar el listado de preguntas
         ordering = ['id', '-pub_date']
 
+    # Implementamos el método __str__
     def __str__(self):
         return 'Pregunta: {}'.format(self.name)
